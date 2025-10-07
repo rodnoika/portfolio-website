b2eb985850719842c4a1144d51200d946ef5ae2b
rodnoi <coolrodnoi@mail.ru>
2025-10-07 19:51:35 +0500

    new

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Yerassyl Portal",
+  description: "Hello everyone! I'm Yerassyl!",
   navItems: [
     {
       label: "Projects",
